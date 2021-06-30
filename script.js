936cfdbf2f30b4356ba40c102fca47b81406527d
somir1 <leonsomir@gmail.com>
2021-06-29 19:04:08 -0500

    dojo weather functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+var close = document.querySelector("#cookiecon")
+
+function remove(element){
+    console.log(element);
+    var el = document.getElementById(element)
+    console.log(el);
+    el.remove()
+}
