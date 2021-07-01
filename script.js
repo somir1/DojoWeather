f870dfcd771213eb4f28c0a11de5cd589795d6fd
somir1 <leonsomir@gmail.com>
2021-07-01 13:02:12 -0500

    F to C convert

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 var close = document.querySelector("#cookiecon")
 
-function remove(element){
+function remove(element) {
     console.log(element);
     var el = document.getElementById(element)
     console.log(el);
     el.remove()
+}
+
+var unit = document.querySelector('#degrees').value
+var temps = document.getElementsByClassName('tempnum')
+
+for(var i = 0; i < temps.length; i++){
+    console.log(temps[i].innerHTML)
+}
+
+
+function changeUnit(element) {
+    unit = element.value
+    convertF(unit)
+}
+
+function convertF(unit) {
+
+    if(unit == 'F'){
+        for(var i = 0; i < temps.length; i++){
+            temps[i].innerHTML = Math.round(((temps[i].innerHTML * 1.8) + 32) * 10) /10
+        }
+    } else{
+        for(var i = 0; i < temps.length; i++){
+            
+            temps[i].innerHTML = Math.round(((temps[i].innerHTML - 32)  * .5556)* 10)/10
+        }
+    }
+
+
 }
